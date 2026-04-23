d0caaf9a020cf38f0206692035d2ac2075bdeb29
Skye J <skye@skyenet.online>
2026-04-23 15:22:48 -0400

    add and then remove opencode

--- ai/default.nix
+++ ai/default.nix
@@ -1,12 +1,57 @@
 {
   pkgs,
-inputs,
+  inputs,
   ...
 }:
 let
   claude-prompt = inputs.claude-prompt.packages."${pkgs.stdenv.hostPlatform.system}".default;
 in
 {
+  # UHHHHHHH this might be causing department chargebacks maybe don't do that.
+  programs.opencode = {
+    enable = false;
+    settings = {
+      provider = {
+        litellm = {
+          npm = "@ai-sdk/openai-compatible";
+          name = "LiteLLM";
+          options = {
+            baseURL = "";
+          };
+          models = {
+            "Claude Sonnet 4.6" = {
+              name = "Claude Sonnet 4.6";
+            };
+            "gemini-3-flash-preview" = {
+              name = "gemini-3-flash-preview";
+            };
+            "GPT-5.4-nano" = {
+              name = "GPT-5.4-nano";
+            };
+            "gpt-oss:120b" = {
+              name = "gpt-oss:120b";
+            };
+            "Claude Haiku 4.5" = {
+              name = "Claude Haiku 4.5";
+            };
+            "llama4:16x17b" = {
+              name = "llama4:16x17b";
+            };
+            "QWEN3-Next-Coder 80B" = {
+              name = "QWEN3-Next-Coder 80B";
+            };
+            "Llama4 Maverick" = {
+              name = "Llama4 Maverick";
+            };
+            "GPT-5.4-mini" = {
+              name = "GPT-5.4-mini";
+            };
+          };
+        };
+      };
+    };
+  };
+
   programs.claude-code = {
     enable = true;
     settings = {

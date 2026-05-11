eff7aa21174bf1ef1b04147c9226b547d8524485
Skye J <skye@skyenet.online>
2026-05-11 10:06:36 -0400

    fix: swap type for hash and pipe error

--- files/scripts/askrm.sh
+++ files/scripts/askrm.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if type trash ; then
+if hash trash 2>/dev/null; then
 # shellcheck disable=SC2068 # Since $@ is being treated as command argument, we want to keep it.
   read -r -p "Are you sure you didn't mean trash? (y/N) " a && [[ "$a" =~ ^[Yy]$ ]] && rm $@
 else

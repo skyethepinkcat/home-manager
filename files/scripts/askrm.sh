cb518a69a6d845c457c9b16124c9c767f57ead09
Skye J <skye@skyenet.online>
2026-05-11 10:08:05 -0400

    fix: i dont actually care if the user types anything after the y

--- files/scripts/askrm.sh
+++ files/scripts/askrm.sh
@@ -2,7 +2,7 @@
 
 if hash trash 2>/dev/null; then
 # shellcheck disable=SC2068 # Since $@ is being treated as command argument, we want to keep it.
-  read -r -p "Are you sure you didn't mean trash? (y/N) " a && [[ "$a" =~ ^[Yy]$ ]] && rm $@
+  read -r -p "Are you sure you didn't mean trash? (y/N) " a && [[ "$a" =~ ^[Yy] ]] && rm $@
 else
   rm $@
 fi

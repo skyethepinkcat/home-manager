2c3c2b7ceaa37d5dadcaa6b84bfff5f50de71a90
Skye J <skye@skyenet.online>
2026-08-20 12:23:34 -0400

    feat: update combinepdfs

--- files/scripts/combine_pdfs.sh
+++ files/scripts/combine_pdfs.sh
@@ -5,7 +5,7 @@ DIR="${1:-.}"
 OUT="${2:-combined.pdf}"
 
 echo "$PATH"
-mapfile -d '' pdfs < <(find "$DIR" -type f -iname '*.pdf' -print0 | grep -zZv 'US Bank' | grep -zZv 'peoplesoft'| sort -z)
+mapfile -d '' pdfs < <(find "$DIR" -type f -iname '*.pdf' -print0 | grep -izZv 'US Bank' | grep -izZv 'peoplesoft'| sort -z)
 
 if [ "${#pdfs[@]}" -eq 0 ]; then
   echo "No PDFs found in $DIR" >&2
